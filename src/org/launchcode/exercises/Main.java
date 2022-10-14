6a0788062dfa10d9be018e6cc1a1987be05ce66e
FadirTorcer <eric.lyonford@gmail.com>
2022-10-14 06:38:11 -0500

    Working on exceptions exercise. Still defining Divide()

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -2,12 +2,24 @@ package org.launchcode.exercises;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        int pointsEarned;
+        int pointsTotal;
+        double score;
+        Scanner input = new Scanner(System.in);
+
         // Test out your Divide() function!
+        System.out.println("Please enter the total number of earned points: ");
+        pointsEarned = input.nextInt();
+        System.out.println("Please enter the total number of possible points for this assignment: ");
+        pointsTotal = input.nextInt();
+        score = Divide(pointsEarned, pointsTotal) * 100;
+        System.out.println("The student's assignment score is : " + score + " percent.");
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -21,11 +33,12 @@ public class Main {
     public static void Divide(int x, int y)
     {
         // Write code here!
+
     }
 
-    public static int CheckFileExtension(String fileName)
+/*    public static int CheckFileExtension(String fileName)
     {
         // Write code here!
-    }
+    }*/
 
 }
