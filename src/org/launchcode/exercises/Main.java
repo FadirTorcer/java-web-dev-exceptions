cc59c65b59aa080056791c344c7983a8636c1714
FadirTorcer <eric.lyonford@gmail.com>
2022-10-15 01:16:47 -0500

    Finished exception exercises.

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -21,6 +21,8 @@ public class Main {
         score = Divide(pointsEarned, pointsTotal) * 100;
         System.out.println("The student's assignment score is : " + score + " percent.");
 
+        // ---<<<>>>---
+
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
@@ -28,17 +30,40 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        System.out.println("Checking submitted files...");
+        for (Map.Entry<String, String> entry: studentFiles.entrySet()) {
+            System.out.println(entry.getKey() + " submitted " + entry.getValue());
+            CheckFileExtension(entry.getValue());
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static double Divide(int x, int y)
     {
         // Write code here!
-
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You can't divide by zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return (double) x/y;
     }
 
-/*    public static int CheckFileExtension(String fileName)
+   public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
-    }*/
+       // Write code here!
+       if (fileName == null || fileName == "") {
+            try {
+                throw new Exception("Please submit, like, a real file?");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return -1;
+        } else if (fileName.endsWith(".java")) {
+            return 1;
+       }
+       return 0;
+    }
 
 }
